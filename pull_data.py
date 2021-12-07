470967a13ae52d7f8875d63fffb54ef51111deba
Daxin Niu <daxinniu111@gmail.com>
2021-12-07 00:28:50 -0500

    pull

--- pull_data.py
+++ pull_data.py
@@ -1,7 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
-
+import csv
 from google.cloud import storage
+
+URL = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports_us/01-01-2021.csv"
+
+with requests.Session() as s:
+    download = s.get(URL)
+
+    decoded_content = download.content.decode('utf-8')
+
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
+    for row in my_list:
+        print(row)
 
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
