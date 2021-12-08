f149eb9b08e774ed3af7ce1690301455bd25ccd3
Wendy Huai <wendyhuai@g.ucla.edu>
2021-12-08 16:30:16 -0500

    one graph ready

--- app.py
+++ app.py
@@ -3,78 +3,96 @@ from dash import dcc
 from dash import html
 from dash import dash_table
 from dash.dependencies import Input, Output, State
-import dash_pivottable
+#import dash_pivottable
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
+import numpy as np
+from pull_data import pull_data
+from pull_data import clean_data
 
-df = pd.read_csv('https://bit.ly/elements-periodic-table')
-def identity(x): return x
+# Load Data
+URL = "https://api.covidactnow.org/v2/states.csv?apiKey=bd3dfdfc355042c0996d60494588c092"
+historical = "https://api.covidactnow.org/v2/states.timeseries.csv?apiKey=bd3dfdfc355042c0996d60494588c092"
 
-df2 = df.pivot_table(
-    index='Period',
-    columns='Group', 
-    values='Element',
-    aggfunc=identity,
-)
-df3 = pd.DataFrame(df2.to_records())
+historical_data = pull_data(historical)
+historical_data = clean_data(historical_data)
 
+
+#APP
 app = dash.Dash(__name__)
 
 
 app.layout = html.Div([
-    html.H2("Pivot Table"),
-    html.H4("Index"),
+    html.H2("COVID Data Visualization"),
+    html.H4("State"),
     dcc.Dropdown(
-        id="index-dropdown",
-        options=[{'label': i, 'value': i} for i in df.columns],
+        id="state",
+        options=[{'label': i, 'value': i} for i in np.sort(historical_data.State.unique()) ],
         multi=False,
-        value="Period"
+        value="RI"
     ),
-    html.H4("Columns"),
+    html.H4("Option"),
     dcc.Dropdown(
-        id="columns-dropdown",
-        options=[{'label': i, 'value': i} for i in df.columns],
+        id="option",
+        options=[{'label': i, 'value': i} for i in historical_data.columns[4:] ],
         multi=False,
-        value="Group"
+        value="Number of Cases"
     ),
-    html.H4("Values"),
-    dcc.Dropdown(
-        id="values-dropdown",
-        options=[{'label': i, 'value': i} for i in df.columns],
-        multi=False,
-        value="Element"
-    ),
-    html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
-    dash_table.DataTable(
-    id='table',
-    columns=[{"name": i, "id": i} for i in df3.columns],
-    data=df3.to_dict('records'),
-    )
+    html.Br(),
+    dcc.Graph(id='line_plot', figure={})
+    # html.H4("Values"),
+    # dcc.Dropdown(
+    #     id="values-dropdown",
+    #     options=[{'label': i, 'value': i} for i in df.columns],
+    #     multi=False,
+    #     value="Element"
+    # ),
+    # html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
+    # dash_table.DataTable(
+    # id='table',
+    # columns=[{"name": i, "id": i} for i in df3.columns],
+    # data=df3.to_dict('records'),
+    # )
 ])
 
-@app.callback([
-    Output('table', 'data'),
-    Output('table', 'columns')
-    ], 
-    [
-        Input('submit-button-state', 'n_clicks'),
-        State('index-dropdown', 'value'),
-        State('columns-dropdown', 'value'),
-        State('values-dropdown', 'value'),
-    ])
-def update_cols(n_clicks, index_val, columns_val, values_val):
-    if index_val == columns_val or index_val == values_val or values_val == columns_val:
-        print("Inputs must all be different!")
-        return None, None
-    temp = df.pivot_table(
-        index=index_val,
-        columns=columns_val, 
-        values=values_val,
-        aggfunc=identity,
-    )
-    out_df = pd.DataFrame(temp.to_records())
-    out_cols = [{"name": i, "id": i} for i in out_df]
-    return out_df.to_dict('records'), out_cols
+
+@app.callback(
+    [Output(component_id='line_plot', component_property='figure')],
+    [Input(component_id='option', component_property='value')]
+)
+def update_graph( option_val):
+    
+    # Plotly Express
+    fig = px.line(historical_data, x='Date', y= option_val , color = 'State', title = f"{option_val} Overview")
+    
+    return [go.Figure(data=fig)]
+
+
+# @app.callback([
+#     # Output('table', 'data'),
+#     # Output('table', 'columns')
+#     # ], 
+#     # [
+#     #     Input('submit-button-state', 'n_clicks'),
+#     #     State('index-dropdown', 'value'),
+#     #     State('columns-dropdown', 'value'),
+#     #     State('values-dropdown', 'value'),
+#     ])
+
+# def update_cols(n_clicks, index_val, columns_val, values_val):
+#     if index_val == columns_val or index_val == values_val or values_val == columns_val:
+#         print("Inputs must all be different!")
+#         return None, None
+#     temp = df.pivot_table(
+#         index=index_val,
+#         columns=columns_val, 
+#         values=values_val,
+#         aggfunc=identity,
+#     )
+#     out_df = pd.DataFrame(temp.to_records())
+#     out_cols = [{"name": i, "id": i} for i in out_df]
+#     return out_df.to_dict('records'), out_cols
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- pull_data.py
+++ pull_data.py
@@ -1,12 +1,14 @@
-from bs4 import BeautifulSoup
 import requests
 import csv
-from google.cloud import storage
+#from google.cloud import storage
 import pandas as pd
+import numpy as np
+
 
 
 URL = "https://api.covidactnow.org/v2/states.csv?apiKey=bd3dfdfc355042c0996d60494588c092"
 historical = "https://api.covidactnow.org/v2/states.timeseries.csv?apiKey=bd3dfdfc355042c0996d60494588c092"
+
 def pull_data(URL):
     response = requests.get(URL)
     decoded_content = response.content.decode('utf-8')
@@ -23,52 +25,98 @@ def pull_data(URL):
     df.to_csv(file_name, index = False, header=False)
     return df
 
-df = pull_data(URL)
-#print(str(df["lastUpdatedDate"][0])+".csv")
+
+cols = ['date', 'country', 'state', 'fips', 'actuals.cases', 'actuals.deaths',
+       'actuals.positiveTests', 'actuals.negativeTests',
+       'actuals.contactTracers', 'actuals.hospitalBeds.capacity',
+       'actuals.hospitalBeds.currentUsageTotal',
+       'actuals.hospitalBeds.currentUsageCovid', 
+       'actuals.icuBeds.capacity', 'actuals.icuBeds.currentUsageTotal',
+       'actuals.icuBeds.currentUsageCovid', 'actuals.newCases',
+       'actuals.vaccinesDistributed', 'actuals.vaccinationsInitiated',
+       'actuals.vaccinationsCompleted', 'metrics.testPositivityRatio',
+        'metrics.caseDensity',
+       'metrics.contactTracerCapacityRatio', 'metrics.infectionRate',
+       'metrics.infectionRateCI90', 
+       'metrics.icuCapacityRatio', 'riskLevels.overall',
+       'metrics.vaccinationsInitiatedRatio',
+       'metrics.vaccinationsCompletedRatio', 'actuals.newDeaths',
+       'actuals.vaccinesAdministered', 'riskLevels.caseDensity',
+       'cdcTransmissionLevel']
+
+col_names = ['Date', 'Country', 'State', 'fips', 
+        'Number of Cases', 'Number of Deaths',
+        'Number of Positive Tests', 'Number of Negative Tests',
+        'Contact Tracers', 'Hhospital Beds Capacity',
+        'Hospital Beds Current Usage Total',
+       'Hospital Beds Current Usage Covid', 
+       'ICU Beds Capacity', 'ICU Beds Current Usage Total',
+       'ICU Beds Current Usage Covid', 'Number of New Cases',
+       'Vaccines Distributed', 'Vaccinations Initiated',
+       'Vaccinations Completed', 'Test Positivity Ratio',
+        'Case Density',
+       'Contact Tracer Capacity Ratio', 'Infection Rate',
+       'Infection Rate CI90', 
+       'ICU Capacity Ratio', 'Risk Levels Overall',
+       'Vaccinations Initiated Ratio',
+       'Vaccinations Completed Ratio', 'New Deaths',
+       'Vaccines Administered', 'Risk Levels - Case Density',
+       'CDC Transmission Level']
+
+def clean_data(historical_data):
+
+    historical_data = historical_data[cols]
+    historical_data.columns = col_names
+    historical_data = historical_data.replace('', np.nan)
+
+    temp = historical_data.iloc[:, 4:].copy()
+    temp = temp.astype(float)
+    
+    historical_data.iloc[:, 4:] = temp
+    return historical_data
+
+
+# """    
+# with requests.Session() as s:
+#     download = s.get(URL)
+#     decoded_content = download.content.decode('utf-8')
+#     cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+#     my_list = list(cr)
+#     fields = my_list[0]
+#     rows = my_list[1:]
 
 
 
-"""    
-with requests.Session() as s:
-    download = s.get(URL)
-    decoded_content = download.content.decode('utf-8')
-    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-    my_list = list(cr)
-    fields = my_list[0]
-    rows = my_list[1:]
+# def upload_blob(bucket_name, source_file_name, destination_blob_name):
+#     # bucket_name = "your-bucket-name"
+#     # source_file_name = "local/path/to/file"
+#     # destination_blob_name = "storage-object-name"
+
+#     storage_client = storage.Client()
+#     bucket = storage_client.bucket(bucket_name)
+#     blob = bucket.blob(destination_blob_name)
+
+#     blob.upload_from_filename(source_file_name)
 
 
+# # Setting credentials using the downloaded JSON file
 
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
-    # bucket_name = "your-bucket-name"
-    # source_file_name = "local/path/to/file"
-    # destination_blob_name = "storage-object-name"
+# client = storage.Client.from_service_account_json(json_credentials_path='credentials-python-storage.json')
 
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
+# # Creating bucket object
 
-    blob.upload_from_filename(source_file_name)
+# bucket = client.get_bucket('py-python')
 
+# # Name of the object to be stored in the bucket
+# object_name_in_gcs_bucket = bucket.blob('my_first_gcs_upload.png')
 
-# Setting credentials using the downloaded JSON file
-
-client = storage.Client.from_service_account_json(json_credentials_path='credentials-python-storage.json')
-
-# Creating bucket object
-
-bucket = client.get_bucket('py-python')
-
-# Name of the object to be stored in the bucket
-object_name_in_gcs_bucket = bucket.blob('my_first_gcs_upload.png')
-
-# Name of the object in local file system
-object_name_in_gcs_bucket.upload_from_filename(my_list)
+# # Name of the object in local file system
+# object_name_in_gcs_bucket.upload_from_filename(my_list)
 
 
-from google.cloud import storage
-from google.cloud.storage.bucket import Bucket
-client = storage.Client()
-bucket = Bucket.from_string("gs://bucket", client=client)
+# from google.cloud import storage
+# from google.cloud.storage.bucket import Bucket
+# client = storage.Client()
+# bucket = Bucket.from_string("gs://bucket", client=client)
 
-"""
+# """
