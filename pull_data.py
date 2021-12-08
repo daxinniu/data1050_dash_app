9b287609be809328c3eddc6c59704d4d4f4c25d5
Daxin Niu <daxinniu111@gmail.com>
2021-12-07 21:01:30 -0500

    pull

--- pull_data.py
+++ pull_data.py
@@ -2,21 +2,31 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 from google.cloud import storage
+import pandas as pd
 
-URL = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports_us/01-01-2021.csv"
 
+URL = "https://api.covidactnow.org/v2/states.csv?apiKey=bd3dfdfc355042c0996d60494588c092"
+response = requests.get(URL)
+decoded_content = response.content.decode('utf-8')
+cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+my_list = list(cr)
+df = pd.DataFrame(my_list)
+df.to_csv("data.csv", index = False, header=False)
+print(df)
+
+
+"""    
 with requests.Session() as s:
     download = s.get(URL)
-
     decoded_content = download.content.decode('utf-8')
-
     cr = csv.reader(decoded_content.splitlines(), delimiter=',')
     my_list = list(cr)
-    for row in my_list:
-        print(row)
+    fields = my_list[0]
+    rows = my_list[1:]
+
+
 
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
-    """Uploads a file to the bucket."""
     # bucket_name = "your-bucket-name"
     # source_file_name = "local/path/to/file"
     # destination_blob_name = "storage-object-name"
@@ -26,3 +36,26 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     blob = bucket.blob(destination_blob_name)
 
     blob.upload_from_filename(source_file_name)
+
+
+# Setting credentials using the downloaded JSON file
+
+client = storage.Client.from_service_account_json(json_credentials_path='credentials-python-storage.json')
+
+# Creating bucket object
+
+bucket = client.get_bucket('py-python')
+
+# Name of the object to be stored in the bucket
+object_name_in_gcs_bucket = bucket.blob('my_first_gcs_upload.png')
+
+# Name of the object in local file system
+object_name_in_gcs_bucket.upload_from_filename(my_list)
+
+
+from google.cloud import storage
+from google.cloud.storage.bucket import Bucket
+client = storage.Client()
+bucket = Bucket.from_string("gs://bucket", client=client)
+
+"""
